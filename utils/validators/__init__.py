a007ca9c55f4ad89022b34aa11e18dfcf6a65cd0
Aramazd Apujanyan <apujanyan04@gmail.com>
2024-05-28 21:42:52 +0400

    utilities

--- utils/validators/__init__.py
+++ utils/validators/__init__.py
@@ -3,3 +3,4 @@ from .positive_number import Integer, Number
 from .valid_email import Email
 from .date_and_time import DateTime
 from .type_validator import validate_type
+from .custom_validator import CustomValidator
